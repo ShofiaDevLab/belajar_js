862dc2f1124e8def6ebc08b4c428025fe8aeaf83
Dedi Supriadi <ghostshell@outlook.co.id>
2025-03-02 16:23:57 +0800

    menambahkan materi dan tugas while loop

---
+++ belajar2.js
@@ -0,0 +1,16 @@
+
+// contoh perulangan while 
+i = 1;
+while (i <= 5) { 
+  console.log('Hello World');
+  i++;
+}
+
+// buatlah contoh perulangan while dengan menggunakan hello world sebanyak 5 kali
+// dan tambahkan angka 1-5 di setiap hello world
+
+i = 1;
+while (i <= 5) {
+  console.log('Hello World ' + i);
+  i++;
+}
---
+++ tugas1.js
@@ -0,0 +1,12 @@
+// tugas 1
+// Jika kalian memiliki 10 angkot, buatlah perulangan untuk angkot nomor 1 hingga 10 menggunakan while loop. Output yang diharapkan adalah sebagai berikut:
+// Expected output:
+// Angkot No. 1 beroperasi dengan baik
+// Angkot No. 2 beroperasi dengan baik dst.
+
+// Code disini
+i = 1;
+while (i <= 10) {
+  console.log('Angkot No. ' + i + ' beroperasi dengan baik');
+  i++;
+}
