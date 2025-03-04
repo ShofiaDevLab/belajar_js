105ba98baaaa1eb416e90d42887f2c8cb092e74e
Dedi Supriadi <ghostshell@outlook.co.id>
2025-03-04 14:05:30 +0800

    belajar pop up

--- belajar1.js
+++ belajar1.js
@@ -1,5 +1,5 @@
-// alert('Hello World'); // Menampilkan pesan Hello World
+alert('Hello World'); // Menampilkan pesan Hello World
 
-// let nama = prompt('Masukkan nama anda: '); // Memunculkan dialog input nama
-// alert('Halo ' + nama); // Menampilkan pesan Halo nama
-// confirm('Apakah anda yakin keluar?'); // Menampilkan dialog konfirmasi keluar
+let nama = prompt('Masukkan nama anda: '); // Memunculkan dialog input nama
+alert('Halo ' + nama); // Menampilkan pesan Halo nama
+confirm('Apakah anda yakin keluar?'); // Menampilkan dialog konfirmasi keluar
